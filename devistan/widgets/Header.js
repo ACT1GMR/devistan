0d7a18a956824bc5b39c44a8eeb79c0fb0f3115b
Saeed Alipoor <alipoor.sa@gmail.com>
2013-06-17 15:07:55 +0430

    header get modified

--- devistan/widgets/Header.js
+++ devistan/widgets/Header.js
@@ -10,8 +10,11 @@ define([
     "dojo/text!./templates/loginForm.html",
     "dojo/text!./templates/registerForm.html"
 ], function (declare, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, ContentPane, fx, domStyle, template, loginForm, registerForm) {
-    commonLabel = {register: "ثبت نام", login: "ورود", username: "نام کاربری", password: "رمز عبور", cancel: "لغو",userLogin : "ورود کاربران"};
-    cssProps = {formsHeight: 140};
+    commonLabel = {register: "ثبت نام", login: "ورود", username: "نام کاربری",
+        password: "رمز عبور", cancel: "لغو",userLogin : "ورود کاربران",email:"ایمیل",
+        userRegistration:"ثبت نام کاربران"};
+
+    cssProps = {formsHeight: 180};
     return declare([_WidgetBase, _TemplatedMixin , _WidgetsInTemplateMixin], {
         templateString: template,
         baseClass: "header",
@@ -48,7 +51,41 @@ define([
             }
         }),
         registerFormTemplate: declare([_WidgetBase , _TemplatedMixin], {
-            templateString: registerForm
+            templateString: registerForm ,
+
+
+            userRegistration: commonLabel.userRegistration,
+            username:commonLabel.username,
+            email: commonLabel.email,
+            password: commonLabel.password,
+            register : commonLabel.register,
+            _isPasswordIsCleared: false,
+            _isUsernameIsCleared: false,
+            _isEmailIsCleared: false,
+
+            _clearOnFocus: function (e, type) {
+                if ((type === "login" && !this._isUsernameIsCleared) ||
+                    (type === "password" && !this._isPasswordIsCleared) ||
+                    (type === "email" && !this._isEmailIsCleared))
+                    e.target.value = "";
+
+            },
+            postCreate: function () {
+                var self = this;
+                this.registerUsernameNode.onfocus = function (e) {
+                    self._clearOnFocus(e, "login");
+                    self._isUsernameIsCleared = true;
+                };
+                this.registerEmailNode.onfocus = function (e) {
+                    self._clearOnFocus(e, "email");
+                    self._isEmailIsCleared = true;
+                };
+                this.registerPasswordNode.onfocus = function (e) {
+                    self._clearOnFocus(e, "password");
+                    self._isPasswordIsCleared = true;
+                }
+            }
+
         }),
         _loginClick: function (e) {
             if (this._loginButtonState === "not-clicked") {
@@ -81,10 +118,11 @@ define([
             }
         },
         _showHideSlide: function (state) {
+            var self = this;
             if (state === "not-clicked")
                 fx.slideTo({ node: this.domNode, top: cssProps.formsHeight, duration: 200}).play();
             else {
-                fx.slideTo({ node: this.domNode, top: 0, duration: 200 }).play();
+                fx.slideTo({ node: this.domNode, top: 0, duration: 200,onEnd:function(){self._destroySubWidget()} }).play();
             }
         },
         _destroySubWidget: function () {
